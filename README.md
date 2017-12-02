de7ee1637f0e48a885163d532ff6a67259812fea
Ash Browning <ambrowning89@gmail.com>
2017-12-02 19:47:00 +0000

    feat(init): initialising repository
    Basically a copy from last year

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent Of Code 2017 Solutions
+
+### JavaScript
+
+Solutions to the [Advent of Code](http://adventofcode.com/2017/) puzzles written in JavaScript.
---
+++ test.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+/* global describe it expect */
+
+const launcher = require('./launcher.js');
+
+describe('Advent of code tests', () => {
+  // it('should give correct answers to day 1 part 1', () => {
+  //   expect(launcher(1, 1)).toBe(241);
+  // });
+});
